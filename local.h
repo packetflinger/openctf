95cb110d600ca98f4a9275c6cd4d7127743e7c53
Joe Reid <joe@joereid.com>
2023-09-01 21:09:23 -0500

    renamed menu.h

--- local.h
+++ local.h
@@ -34,7 +34,7 @@
    because we define the full size ones in this file */
 #define GAME_INCLUDE
 #include "game.h"
-#include "menu.h"
+#include "p_menu.h"
 
 /* the "gameversion" client command will print this plus compile date */
 #define GAMEVERSION "ctf"
