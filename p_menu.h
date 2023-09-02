f7b3465e640f79588d9816f7f0f8066d4c9419ee
Joe Reid <joe@joereid.com>
2023-09-02 17:50:08 -0500

    formatting

--- p_menu.h
+++ p_menu.h
@@ -22,45 +22,35 @@
  * Header for the CTF ingame menu.
  *
  * =======================================================================
- */ 
+ */
 
 #ifndef CTF_MENU_H
 #define CTF_MENU_H
 
-enum
-{
-	PMENU_ALIGN_LEFT,
-	PMENU_ALIGN_CENTER,
-	PMENU_ALIGN_RIGHT
+enum {
+    PMENU_ALIGN_LEFT, PMENU_ALIGN_CENTER, PMENU_ALIGN_RIGHT
 };
 
-typedef struct pmenuhnd_s
-{
-	struct pmenu_s *entries;
-	int cur;
-	int num;
-	void *arg;
+typedef struct pmenuhnd_s {
+    struct pmenu_s *entries;
+    int cur;
+    int num;
+    void *arg;
 } pmenuhnd_t;
 
 typedef void (*SelectFunc_t)(edict_t *ent, pmenuhnd_t *hnd);
 
-typedef struct pmenu_s
-{
-	char *text;
-	int align;
-	SelectFunc_t SelectFunc;
+typedef struct pmenu_s {
+    char *text;
+    int align;
+    SelectFunc_t SelectFunc;
 } pmenu_t;
 
-pmenuhnd_t *PMenu_Open(edict_t *ent,
-		pmenu_t *entries,
-		int cur,
-		int num,
-		void *arg);
+pmenuhnd_t* PMenu_Open(edict_t *ent, pmenu_t *entries, int cur, int num,
+        void *arg);
 void PMenu_Close(edict_t *ent);
-void PMenu_UpdateEntry(pmenu_t *entry,
-		const char *text,
-		int align,
-		SelectFunc_t SelectFunc);
+void PMenu_UpdateEntry(pmenu_t *entry, const char *text, int align,
+        SelectFunc_t SelectFunc);
 void PMenu_Do_Update(edict_t *ent);
 void PMenu_Update(edict_t *ent);
 void PMenu_Next(edict_t *ent);
